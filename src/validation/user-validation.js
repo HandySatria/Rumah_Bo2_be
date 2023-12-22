2a3ddbe08c084b35ee59707e819ffaff88aa2b87
HandySatria <handysatria.nayaka@gmail.com>
2023-12-22 15:46:39 +0700

    resolve conflict

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -4,9 +4,13 @@ const registerUserValidation = Joi.object({
   username: Joi.string().max(100).required(),
   full_name: Joi.string().max(150).required(),
   password: Joi.string().max(100).required(),
-  name: Joi.string().max(100).required(),
-  role: Joi.string().max(100).required(),
-});
+  role_code: Joi.string().max(100).required(),
+  employee_id: Joi.number().min(1).positive().optional(),
+  user_crt: Joi.string().max(100).optional(),
+  user_upd: Joi.string().max(100).optional(),
+  dtm_crt: Joi.date().optional(),
+  dtm_upd: Joi.date().optional(),  
+}).options({ abortEarly: false, allowUnknown: false });
 
 const loginUserValidation = Joi.object({
   username: Joi.string().max(100).required(),
@@ -18,10 +22,15 @@ const getUserValidation = Joi.string().max(100).required();
 const updateUserValidation = Joi.object({
   user_id: Joi.number().min(1).positive().required(),
   username: Joi.string().max(100).required(),
-  password: Joi.string().max(100).optional(),
-  name: Joi.string().max(100).optional(),
-  role: Joi.string().max(100).required(),
-});
+  full_name: Joi.string().max(150).required(),
+  password: Joi.string().max(100).required(),
+  role_code: Joi.string().max(100).required(),
+  employee_id: Joi.number().min(1).positive().optional(),
+  user_crt: Joi.string().max(100).optional(),
+  user_upd: Joi.string().max(100).optional(),
+  dtm_crt: Joi.date().optional(),
+  dtm_upd: Joi.date().optional(),
+}).options({ abortEarly: false, allowUnknown: false });
 
 export {
   registerUserValidation,
